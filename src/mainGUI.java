cbb762f755d6f87c3f1bbb9248c8d64ca93e4e8c
BuildTools <unconfigured@null.spigotmc.org>
2022-03-21 00:45:27 +0800

    SUSSY??

--- src/mainGUI.java
+++ src/mainGUI.java
@@ -2,11 +2,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
-
-import javafx.embed.swing.JFXPanel;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
 
 public class mainGUI extends JFrame {
 
@@ -14,22 +9,19 @@ public class mainGUI extends JFrame {
     private JTextField textField;
     private JLabel susPercentage;
     private JButton button;
-    private MediaPlayer mediaPlayer;
+
     public mainGUI(){
+
         setLayout(new FlowLayout());
 
+
         label = new JLabel("Who sus???? ");
-        label.setFont(new Font("Serif", Font.PLAIN, 60));
+        label.setFont(new Font("Serif", Font.PLAIN, 20));
         add(label);
 
         textField = new JTextField(" sus person name here ",15);
-        textField.setFont(new Font("Serif", Font.PLAIN, 55));
+        textField.setFont(new Font("Serif", Font.PLAIN, 20));
         add(textField);
-
-        String alarm = "src/alarm.mp3";
-        Media hit = new Media(new File(alarm).toURI().toString());
-        mediaPlayer = new MediaPlayer(hit);
-
 
         button = new JButton("Check if sussy");
         button.addActionListener(new ActionListener(){
@@ -38,30 +30,26 @@ public class mainGUI extends JFrame {
                 String name = textField.getText();
                 int percentage = (int)(Math.random()*100);
                 susPercentage.setText(percentage + "%");
-                if(percentage > 50) {
+                if(percentage > 50 || percentage == 50) {
                     label.setText(name + " is sussy");
-                    mediaPlayer.play();
                 } else {
                     label.setText(name + " is not sus");
-                    mediaPlayer.stop();
+
                 }
             }
-
         });
         add(button);
-
         susPercentage = new JLabel(" x% ");
-        susPercentage.setFont(new Font("Serif", Font.PLAIN, 100));
+        susPercentage.setFont(new Font("Serif", Font.PLAIN, 40));
         add(susPercentage);
 
     }
-
     public static void main(String[] args){
-        final JFXPanel fxPanel = new JFXPanel();
         mainGUI gui = new mainGUI();
         gui.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        gui.setSize(1500, 700);
+        gui.setSize(255, 300);
         gui.setVisible(true);
         gui.setTitle("Suspicious person verifier");
+        gui.setResizable(false);
     }
 }
